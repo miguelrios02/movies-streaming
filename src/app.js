640dff6e718f4050eaa310baadbfa43020c1a427
mianriga12 <mianriga@gmail.com>
2023-02-24 21:27:53 -0500

    skeleton terminado

--- src/app.js
+++ src/app.js
@@ -1,48 +1,49 @@
-const express = require('express')
+const express = require("express");
 
-const responseHandlers = require('./utils/handleResponses')
-const db = require('./utils/database')
-const initModels = require('./models/initModels')
+const responseHandlers = require("./utils/handleResponses");
+const db = require("./utils/database");
+const initModels = require("./models/initModels");
 
-const userRouter = require('./users/users.router')
-const authRouter = require('./auth/auth.router')
+const userRouter = require("./users/users.router");
+const authRouter = require("./auth/auth.router");
+const config = require("../config").api;
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 db.authenticate()
-    .then(() => console.log('Database authenticated'))
-    .catch(err => console.log(err))
+  .then(() => console.log("Database authenticated"))
+  .catch((err) => console.log(err));
 
 db.sync()
-    .then(() => console.log('Database Synced'))
-    .catch(err => console.log(err))
+  .then(() => console.log("Database Synced"))
+  .catch((err) => console.log(err));
 
-initModels()
+initModels();
 
-app.get('/', (req, res) => {
-    responseHandlers.success({
-        res,
-        status: 200,
-        message: 'Servidor inicializado correctamente',
-        data: {
-            "users": "http://localhost:9000/api/v1/users",
-        }
-    })
-})
+app.get("/", (req, res) => {
+  responseHandlers.success({
+    res,
+    status: 200,
+    message: "Servidor inicializado correctamente",
+    data: {
+      users: `${config.host}/api/v1/users`,
+    },
+  });
+});
 
-app.use('/api/v1/users', userRouter)
-app.use('/api/v1/auth', authRouter)
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/auth", authRouter);
 
-app.use('*', (req, res)=> {
-    responseHandlers.error({
-        res,
-        status: 404,
-        message: 'URL not found, please try with http://localhost:9000/',
-    })
-})
+app.use("*", (req, res) => {
+  responseHandlers.error({
+    res,
+    status: 404,
+    message: `URL not found, please try with ${config.host}`,
+  });
+});
 
-app.listen(9000,() => {
-    console.log('Server started at port 9000')
-})
+app.listen(config.port, () => {
+  console.log(`Server started at port ${config.port}`);
+});
